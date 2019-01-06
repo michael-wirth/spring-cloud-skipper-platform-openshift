e6b707e2abd3810c6fbb409bb190848311039d03
Michael Wirth <michael.wirth@mimacom.com>
2019-01-06 22:46:05 +0100

    fix: add spring-cloud-skipper-server-openshift project

---
+++ spring-cloud-skipper-server-openshift/src/main/java/org/springframework/cloud/dataflow/skipper/openshift/OpenShiftSkipperServer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.skipper.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
+import org.springframework.cloud.skipper.server.EnableSkipperServer;
+import org.springframework.cloud.skipper.server.autoconfigure.CloudFoundryPlatformAutoConfiguration;
+import org.springframework.cloud.skipper.server.autoconfigure.KubernetesPlatformAutoConfiguration;
+
+/**
+ * Bootstrap class for the OpenShift 3 Spring Cloud Skipper Server.
+ *
+ * @author Michael Wirth
+ */
+@SpringBootApplication(exclude = { LocalDeployerAutoConfiguration.class,
+		KubernetesAutoConfiguration.class, KubernetesPlatformAutoConfiguration.class,
+		CloudFoundryPlatformAutoConfiguration.class })
+@EnableSkipperServer
+public class OpenShiftSkipperServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenShiftSkipperServer.class, args);
+	}
+
+}
---
+++ spring-cloud-skipper-server-openshift/src/test/java/org/springframework/cloud/dataflow/skipper/openshift/OpenShiftDataFlowSkipperTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.skipper.openshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.deployer.resource.docker.DockerResource;
+import org.springframework.cloud.deployer.resource.maven.MavenResource;
+import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
+import org.springframework.cloud.deployer.spi.app.AppDeployer;
+import org.springframework.cloud.deployer.spi.openshift.OpenShiftDeployerProperties;
+import org.springframework.cloud.deployer.spi.openshift.ResourceAwareOpenShiftAppDeployer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = OpenShiftSkipperServer.class, properties = {
+		"spring.cloud.deployer.openshift.forceBuild=true",
+		"spring.cloud.deployer.openshift.limits.memory=128Mi" })
+public class OpenShiftDataFlowSkipperTest {
+
+	@Autowired
+	private AppDeployer appDeployer;
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Test
+	public void contextLoads() {
+		assertThat(appDeployer).isInstanceOf(ResourceAwareOpenShiftAppDeployer.class);
+	}
+
+	@Test
+	public void testDeployerProperties() {
+		OpenShiftDeployerProperties properties = context
+				.getBean(OpenShiftDeployerProperties.class);
+		assertThat(properties.isForceBuild()).isTrue();
+		assertThat(properties.getLimits().getMemory()).isEqualTo("128Mi");
+	}
+
+	@Test
+	public void testDockerResource() {
+		DelegatingResourceLoader resourceLoader = context
+				.getBean(DelegatingResourceLoader.class);
+		assertThat(resourceLoader.getResource(
+				"maven://org.springframework.cloud:spring-cloud-dataflow-server-core:1.2.3.RELEASE"))
+						.isInstanceOf(MavenResource.class);
+		assertThat(resourceLoader.getResource("docker://helloworld:latest"))
+				.isInstanceOf(DockerResource.class);
+	}
+
+}
